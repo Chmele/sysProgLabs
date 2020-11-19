18a62f2ddf9e24dabbabe7209e51c61d2b2b6a63
Vsevolod Hermaniuk <53478956+Chmele@users.noreply.github.com>
2020-11-19 21:29:40 +0200

    provide mock, refactor

--- src/main/java/com/company/Solution.java
+++ src/main/java/com/company/Solution.java
@@ -1,43 +1,42 @@
 package com.company;
 
-import java.io.*;
+import java.io.IOException;
 import java.util.HashSet;
-import java.util.Set;
 import java.util.Scanner;
 
-public class Main {
-
-    public static void main(String[] args) throws IOException {
+public class Solution {
+    public FileSystemDataService service;
+    public Solution(){
+        this.service = new FileReaderWrap();
+    }
+    public void setService(FileSystemDataService s){
+        this.service = s;
+    }
+    public HashSet<String> main() throws IOException {
         System.out.println("Enter file path:");
         Scanner input = new Scanner(System.in);
         String filePath = input.nextLine();
-        try {
-            FileReader fr = new FileReader(filePath);
-            int i;
-            StringBuilder sb = new StringBuilder();
-            while ((i=fr.read()) != -1)
-                sb.append((char) i);
-            String text = sb.toString();
-            String[] words = text.split(" |\\r?\\n|\\t");
-
-            slice_words(words);
-
-            filter_words(words);
-        }
-        catch (FileNotFoundException e){
-            System.out.println("No such file");
-        }
+        return process(filePath);
     }
 
-    public static  void slice_words(String[] words){
+    public HashSet<String> process(String path) throws IOException {
+        String text = this.service.getData(path);
+        String[] words = text.split(" |\\r?\\n|\\t");
+
+        slice_words(words);
+
+        return filter_words(words);
+    }
+
+    public void slice_words(String[] words){
         for (int i = 0; i < words.length; i++){
             words[i] = words[i].substring(0, Math.min(words[i].length(), 30));
         }
     }
 
-    public static void filter_words(String[] words){
+    public HashSet<String> filter_words(String[] words){
         int max = 0;
-        Set<String> ret_words = new HashSet<>();
+        var ret_words = new HashSet<String>();
         for (int i = 0; i < words.length; i++) {
             if (countSubConsonant(words[i]) == max){
                 ret_words.add(words[i]);
@@ -48,10 +47,11 @@ public class Main {
                 ret_words.add(words[i]);
             }
         }
-        System.out.println(ret_words.toString());
+//        System.out.println(ret_words.toString());
+        return ret_words;
     }
 
-    public static int countSubConsonant(String word){
+    public int countSubConsonant(String word){
         int count = 0;
         int max = 0;
         String consonantLetters = "BCDFGHJKLMNPQRSTVXZWY";
